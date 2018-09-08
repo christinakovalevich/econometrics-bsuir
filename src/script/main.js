5a6d3137cdb474bafa1d60fedafff652b6109c69
Gleb Remniov <remniov.work@gmail.com>
2018-09-08 11:11:52 +0300

    Script changes

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,10 @@
 $(function () {
-    $('#button_next').click(function () {
-        console.log('btn next was clicked');
-        window.open('src/pages/lection2.html', '_self');
-    });
+    // $('#button_next').click(function () {
+    //     console.log('btn next was clicked');
+    //     window.open('src/pages/lection2.html', '_self');
+    // });
 });
+
+function openPage(href, blank) {
+    window.open(href, blank);
+}
